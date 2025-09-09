20c7e148c4aec6a0abbee83fbc36eeb4d158ae58
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-09-09 20:50:56 +0530

    Fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import Blog from "./pages/Blog"
@@ -11,6 +11,7 @@ function App() {
      <BrowserRouter>
        <Routes>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/blog/:id" element={<Blog/>}/>
         <Route path="/blogs" element={<Blogs/>}/>
